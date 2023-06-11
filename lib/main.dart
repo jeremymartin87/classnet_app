1601b27b267781eef7fe6eb1251c0c448909fe0d
jeremymartin87 <jeremy.martin87620@gmail.com>
2023-06-11 14:19:25 +0200

    1.1.1 Ajout Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
+
+import 'dart:io';
+
 import 'package:classnet_app/boxes.dart';
 import 'package:classnet_app/course/view/all_cours.dart';
 import 'package:classnet_app/model/hive/cours.dart';
 import 'package:classnet_app/model/hive/my_cours.dart';
 import 'package:classnet_app/navbar/bottom_navbar.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Box? box;
 const darkModeBox = 'darkModeTutorial';
 bool darkMode = false;
 
 void main() async {
+  FlutterError.onError = (details) {
+    FlutterError.presentError(details);
+    if (kReleaseMode) exit(1);
+  };
   WidgetsFlutterBinding.ensureInitialized();
+  try {
   await Hive.initFlutter();
   await Hive.openBox(darkModeBox);
   Hive..registerAdapter(CoursAdapter())
@@ -21,6 +31,11 @@ void main() async {
   box = await Hive.openBox<My_Cours>('courbox6');
   await Hive.openBox(darkModeBox);
   await Boxes.initHive();
+  } catch (e,s) {
+    print('Erreur lors de l initialisation de Hive: $e');
+    print(s);
+    return;
+    }
   runApp(const MyApp());
 }
 
